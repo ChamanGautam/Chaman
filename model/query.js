f2b66847db5545594a59746bb713137990d421a1
Chaman Gautam <152677595+ChamanGautam@users.noreply.github.com>
2024-08-27 00:11:40 +0530

    Delete model directory

--- model/query.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const portfolioSchema = new mongoose.Schema({
-    title: String,
-    message: String
-});
-
-module.exports = mongoose.model('portfolio-data', portfolioSchema);
