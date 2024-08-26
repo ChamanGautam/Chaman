e7919a1914de52453e91ce7efe198c67544d5505
Chaman Gautam <152677595+ChamanGautam@users.noreply.github.com>
2024-08-26 15:42:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/getQuery', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4500;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
